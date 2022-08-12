033d277d3cdb3454fbcf992dd5624ac92bec03d0
Suket Kamboj <82956207+Sookeyy-12@users.noreply.github.com>
2022-08-12 16:09:39 +0530

    made the board for the game

--- betterGame.py
+++ betterGame.py
@@ -1,1 +1,24 @@
+# creating 7x6 column for the game using a 2D list
+playSpace =[[' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' ']]
 
+#printing top of the board to indicate the column number
+columnIndicator = ' '
+for n in range(1, len(playSpace)+1):
+  columnIndicator += ' ' + str(n) + '  '
+print(columnIndicator)
+print("+---"*len(playSpace) + "+")
+
+#printing rows and columns of the board
+for row in range(len(playSpace[0])):
+    print('|   ' * (len(playSpace) +1))
+    row_with_items = ''
+    for col in range(len(playSpace)):
+        row_with_items += ('| '+str(playSpace[col][row])) + ' '
+    print(row_with_items + '|')
+    print('|   ' * (len(playSpace) +1))
+    print('+---' * (len(playSpace)) + '+')
