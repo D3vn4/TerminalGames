718b97c2528004880feea7ca359c408ef0498235
D3VN4 <devnasharma1903@gmail.com>
2024-06-27 02:10:37 +0530

    Do not read this commit shoo

--- TicTacToe/C++/ttt.cpp
+++ TicTacToe/C++/ttt.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include "ttt.hpp"
-
-
-
-
-
+using namespace std;
 int main() {
     greet();
     startGame();
-
+    cout<<"I have hacked this game HAHAHAHAHAHAHA";
     return 0;
 }
